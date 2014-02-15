9993373556019cde267e72045eb17e498f04bc55
Jeremy Ong <jeremycong@gmail.com>
2014-02-14 19:05:05 -0800

    Rename source file.

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -1,29 +1,6 @@
-#include "util.h"
+#include "primitives.h"
 
 namespace sel {
-std::ostream &operator<<(std::ostream &os, lua_State *l) {
-    int top = lua_gettop(l);
-    for (int i = 1; i <= top; ++i) {
-        int t = lua_type(l, i);
-        switch(t) {
-        case LUA_TSTRING:
-            os << lua_tostring(l, i);
-            break;
-        case LUA_TBOOLEAN:
-            os << (lua_toboolean(l, i) ? "true" : "false");
-            break;
-        case LUA_TNUMBER:
-            os << lua_tonumber(l, i);
-            break;
-        default:
-            os << lua_typename(l, t);
-            break;
-        }
-        os << " ";
-    }
-    return os;
-}
-
 namespace detail {
 void _push(lua_State *l, bool b) {
     lua_pushboolean(l, b);
